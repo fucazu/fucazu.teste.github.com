6d6840a3d747c77d03f9dce043fdaf2350246d2c
Rafael Sanches <fucazu@gmail.com>
2018-02-05 18:24:06 -0200

    Testando

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+* {
+  font-family: 'Raleway', sans-serif;
+  font-size: 28px;
+}
+body, html, #app {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: black;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="texto">
+    <span>{{ name }}</span>
+    Desenvolvedor de sistemas apaixonado pelo que faz
+    <a :href="github">GitHub</a>
+    <a :href="gitlab">GitLab</a>
+    <a :href="linkedin"><img src="../assets/Logo-White-128px-TM.png" width="150"/></a>
+    <a class="email" href="mailto:contato@fucazu.com">contato@fucazu.com</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'Rafael Sanches',
+      github: 'https://github.com/fucazu',
+      gitlab: 'https://gitlab.com/fucazu',
+      linkedin: 'https://www.linkedin.com/in/rafaelsanchesdev/'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.texto {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: nowrap;
+  color: white;
+  flex-direction: column;
+  margin: auto;
+  text-align: center;
+  span {
+    font-size: 50px;
+  }
+  a {
+    text-decoration: none;
+    background-image: none;
+    color: white;
+    min-width: 150px;
+    font-size: 43px;
+    font-family: 'Roboto Condensed', sans-serif;
+    &.email {
+      font-size: 30px;
+    }
+  }
+}
+</style>
